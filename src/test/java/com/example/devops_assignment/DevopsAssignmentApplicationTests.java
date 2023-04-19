52aeb2d9b1f85872ddd1d37ccd55edb9f48a3009
simrangawri <42602088+simrangawri@users.noreply.github.com>
2023-04-19 21:39:09 +0530

    Add files via upload

---
+++ src/test/java/com/example/devops_assignment/DevopsAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
